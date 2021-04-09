714ebb53f3dd71b2297a129b6b736e45cf07d9e2
Laszlo Hampl <laszlo.hampl@gmail.com>
2021-04-09 21:50:19 +0200

    I've worked on the new file

---
+++ week-01/day-4/Week01 day-4/src/Main.java
@@ -0,0 +1,177 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+    public class Main {
+
+        public static void main(String[] args) {
+            // write your code here
+            System.out.println("Hello World");
+            // single line comment
+        /* Multiple
+        line
+        comment
+         */
+        /*
+        System.out.println("Hello Laszlo!");
+        System.out.println("Humpty Dumpty sat on a wall");
+        System.out.println("Humpty Dumpty had a great fall");
+        System.out.println("All the kin's horses and all the king's men.");
+        System.out.println("Couldn't put Humpty together again!");
+        System.out.println("Hello, Esther");
+        System.out.println("Hello, Mary");
+        System.out.println("Hello, Joe");
+
+        int a = 42;
+        System.out.println(a);
+
+        float f = 3.141592f;
+        System.out.println(f);
+
+        String name = "Laszlo Hampl";
+        System.out.println(name);
+
+        int age = 29;
+        System.out.println(age);
+
+        float height = 1.85f;
+        System.out.println(height);
+
+        int b = 13+22;
+        System.out.println(b);
+
+        int c = 13-22;
+        System.out.println(c);
+
+        int d = 22/13;
+        System.out.println(d);
+
+        float e = 22/13f;
+        System.out.println(e);
+
+        int g = 22/13;
+        System.out.println(g);
+
+        int h = 22%13;
+        System.out.println(h); */
+
+        /*
+        int semester = 17;
+        int day = 6;
+
+        int workdays = semester*5;
+        System.out.println(workdays);
+
+        int timeSpentWithCoding = workdays*day;
+        System.out.println("The total hours an average Green Fox attendee spend with coding is "+timeSpentWithCoding + " hours!");
+
+        int workHours = 52;
+
+        int hoursInOneSem = workHours * semester;
+        System.out.println(hoursInOneSem);
+
+
+        System.out.println(hoursInOneSem*timeSpentWithCoding);
+        */
+        /*
+         int favoriteNumber = 13;
+        System.out.println("My favorite number is: "+favoriteNumber);
+
+        int a = 123;
+        int b = 526;
+
+        a = 526;
+        b = 123;
+
+        System.out.println(a);
+
+        System.out.println(b);
+        */
+        /*
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        double BMI = massInKg/(heightInM*2);
+        System.out.println(BMI);
+
+        String name = "Laszlo Hampl";
+        int age  = 29;
+        double height = 1.85;
+        boolean marriageStatus = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(marriageStatus);
+
+
+        int a = 3;
+         a = a+10;
+        System.out.println(a);
+
+        int b = 100;
+        b = b-7;
+
+        System.out.println(b);
+
+        int c = 44;
+        c = c*2;
+        System.out.println(c);
+
+        int d = 125;
+        d = (d*5)/d;
+        System.out.println(d);
+
+        int e = 8;
+        e = e*e*e;
+        System.out.println(e);
+
+        int f1 = 123;
+        int f2 = 345;
+        System.out.println(f1>f2);
+
+        int g1 = 350;
+        int g2 = 200;
+        System.out.println((g2*2)>g1);
+
+
+        int i1 = 10;
+        int i2 = 3;
+        System.out.println(i1>(i2*i2)&&i1<i2*i2*i2);
+
+        int j = 1521;
+        System.out.println();
+        */
+
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            Scanner scanner = new Scanner(System.in);
+            /*
+            System.out.println("What is your name?");
+
+            String name = scanner.nextLine();
+            System.out.println("Welcome " + name +"!");
+
+            System.out.println("How are you doing today?");
+            String question = scanner.nextLine();
+
+            System.out.println("That's great!");
+            */
+
+            System.out.println("Distance in miles: ");
+
+            double mile = scanner.nextDouble();
+            System.out.println("That is: "+ mile*1.60934 +"km.");
+
+            System.out.println("Write a number please: ");
+            int number = scanner.nextInt();
+
+            if (number%2==0){
+                System.out.println();
+            }
+
+
+
+
+        }
+    }

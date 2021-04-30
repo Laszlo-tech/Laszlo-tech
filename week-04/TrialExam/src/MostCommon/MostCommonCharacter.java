e4f63fe75ce51ed4493269d736ab9c3795fa536d
Laszlo Hampl <laszlo.hampl@gmail.com>
2021-04-30 12:22:44 +0200

    add second excersise

--- week-04/TrialExam/src/MostCommon/MostCommonCharacter.java
+++ week-04/TrialExam/src/MostCommon/MostCommonCharacter.java
@@ -1,13 +1,27 @@
 package MostCommon;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
 
 public class MostCommonCharacter {
-Path filePath = Paths("")
 
+  Path filePath = Paths("src/MostCommon/countchar.txt");
 
-  public void getMostCommonChar(){
+  public char mostCommonChar(Path path) {
+    int count = 0;
+    char character = ' ';
+    try {
+      List<String> characterList =
+          Files.readAllLines(filePath); // Make a list of strings from the file.
+      for (int i = 0; i < characterList.size(); i++) {}
 
-  }
+      HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    } catch (IOException e) {
+      System.out.println("File does not exist!");
+    }
+  }return;
 }
---
+++ week-04/TrialExam/src/Pirates/Pirate.java
@@ -0,0 +1,5 @@
+package Pirates;
+
+public class Pirate {
+
+}

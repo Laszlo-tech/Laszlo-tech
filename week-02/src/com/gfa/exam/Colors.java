c5994528985af6aee83db42284a37d9affc29769
Laszlo Hampl <laszlo.hampl@gmail.com>
2021-05-10 22:16:50 +0200

    add arrays excersises

---
+++ week-02/src/com/gfa/exam/Colors.java
@@ -0,0 +1,11 @@
+package com.gfa.exam;
+
+public class Colors {
+  public static void main(String[] args) {
+    String[][] shades = {
+      {"lime", "forest green", "olive", "pale green", "spring green"},
+      {"orange red", "red", "tomato"},
+      {"orchid", "violet", "pink", "hot pink"}
+    };
+  }
+}
